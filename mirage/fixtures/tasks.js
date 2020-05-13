f4c87a5d02a020bcd5ec89a7c583845a03de85ac
Erick Sutil <ericksutil@gmail.com>
2020-05-13 00:46:47 -0300

    Create new route and fake data using ember-data with mirage

---
+++ mirage/fixtures/tasks.js
@@ -0,0 +1,12 @@
+export default [
+  { id: '1',
+      description: 'Lavar o carro',
+      deadline: 'Maio 13',
+      isDone: false
+    }, {
+      id: '2',
+      description: 'Fazer compras',
+      deadline: 'Maio 14',
+      isDone: false
+    }
+  ];
